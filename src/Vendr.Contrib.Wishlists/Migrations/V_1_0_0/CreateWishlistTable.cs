81cdeb52790496abc46c6696045d9d413de2cd5c
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-10-19 17:25:23 +0200

    Update migration to create wishlist table

---
+++ src/Vendr.Contrib.Wishlists/Migrations/V_1_0_0/CreateWishlistTable.cs
@@ -0,0 +1,43 @@
+﻿using Umbraco.Core.Migrations;
+using Umbraco.Core.Persistence.DatabaseModelDefinitions;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Vendr.Contrib.Wishlists.Migrations.V_1_0_0
+{
+    public class CreateWishlistTable : MigrationBase
+    {
+        public CreateWishlistTable(IMigrationContext context)
+            : base(context)
+        { }
+
+        public override void Migrate()
+        {
+            var wishlistTableName = Constants.DatabaseSchema.Tables.Wishlist;
+            var storeTableName = Core.Constants.DatabaseSchema.Tables.Store;
+
+            if (!TableExists(wishlistTableName))
+            {
+                var nvarcharMaxType = SqlSyntax is SqlCeSyntaxProvider
+                    ? "NTEXT"
+                    : "NVARCHAR(MAX)";
+
+                // Create table
+                Create.Table(wishlistTableName)
+                    .WithColumn("id").AsGuid().NotNullable().WithDefault(SystemMethods.NewGuid).PrimaryKey($"PK_{wishlistTableName}")
+                    .WithColumn("storeId").AsGuid().NotNullable()
+                    .WithColumn("orderGuid").AsGuid().NotNullable()
+                    //.WithColumn("customerReference").AsString(255).Nullable()
+                    .WithColumn("name").AsString(255).NotNullable()
+                    .WithColumn("createDate").AsDateTime().NotNullable()
+                    .WithColumn("updateDate").AsDateTime().NotNullable()
+                    .Do();
+
+                // Foreign key constraints
+                Create.ForeignKey($"FK_{wishlistTableName}_{storeTableName}")
+                    .FromTable(wishlistTableName).ForeignColumn("storeId")
+                    .ToTable(storeTableName).PrimaryColumn("id")
+                    .Do();
+            }
+        }
+    }
+}
